a143cb584264200946981dfa16f7c43a6083bef2
Umad Ahmad <53309523+UAhmadSoft@users.noreply.github.com>
2022-01-13 11:30:31 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./styles.css";
+import { photosData } from "./constants/data";
+import Card from "./components/Card/Card";
+import { FixedSizeList } from "react-window";
+import { useCallback } from "react";
+
+export default function App() {
+  const Row = useCallback(({ index, style }) => {
+    const { id, title, thumbnailUrl } = photosData[index] || {};
+
+    return (
+      <div style={style}>
+        <Card id={id} key={id} title={title} thumbnailUrl={thumbnailUrl} />
+      </div>
+    );
+  }, []);
+  return (
+    <div className="App">
+      <div className="cardsContainer">
+        {/* {photosData.map(({ id, title, thumbnailUrl }) => (
+          <Card id={id} key={id} title={title} thumbnailUrl={thumbnailUrl} />
+        ))} */}
+        <FixedSizeList
+          height={400}
+          width={400}
+          itemSize={150}
+          itemCount={photosData.length}
+        >
+          {Row}
+        </FixedSizeList>
+      </div>
+    </div>
+  );
+}
